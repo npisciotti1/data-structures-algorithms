8d108d27edcb98ebb9d234b2b255dfe0ed99ac8f
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-07-07 21:42:18 -0700

    working example

--- insertion-sort/insertion-sort.js
+++ insertion-sort/insertion-sort.js
@@ -1,3 +1,15 @@
 // Where a bubble sort relies on a number of small swaps, insertion sort relies on
 // inserting a single element in the right for a given iteration. Every iteration
 // through the collection leaves a greater segment sorted.
+
+function sort(values) {
+  var length = values.length;
+  for(var i = 1; i < length; ++i) {
+    var temp = values[i];
+    var j = i - 1;
+    for(; j >= 0 && values[j] > temp; --j) {
+      values[j+1] = values[j];
+    }
+    values[j+1] = temp;
+  }
+};
