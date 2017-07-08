b7cd282d31fea88ac3787a6a0a5f5f27916bc56b
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-07-07 21:41:51 -0700

    algorithm desc

---
+++ insertion-sort/insertion-sort.js
@@ -0,0 +1,3 @@
+// Where a bubble sort relies on a number of small swaps, insertion sort relies on
+// inserting a single element in the right for a given iteration. Every iteration
+// through the collection leaves a greater segment sorted.
